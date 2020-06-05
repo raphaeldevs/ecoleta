dc7fd0babd44234893414abac3fa206981fe5edd
Raphael Devs <raphaeldeoliveiracorrea@gmail.com>
2020-06-05 13:05:12 -0300

    inserindo dados na tabela

--- src/database/db.js
+++ src/database/db.js
@@ -12,6 +12,7 @@ db.serialize(() => {
     CREATE TABLE IF NOT EXISTS places (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       image TEXT,
+      name TEXT,
       address TEXT,
       address2 TEXT,
       state TEXT,
@@ -21,7 +22,36 @@ db.serialize(() => {
   `)
 
   // inserir dados na tabela
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
 
+  const values = [
+    "https://images.unsplash.com/photo-1480359014333-3935abd88252?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    "Colectoria",
+    "Uma rua qualquer, Um bairro",
+    "N° 11",
+    "Maranhão",
+    "São Luís",
+    "Resíduos Eletrônicos, Lâmpadas"
+  ]
+
+  function afterInsertData(error) {
+    if (error) return console.log(error)
+
+    console.log("Cadastrado com sucesso")
+    console.log(this)
+  }
+
+  db.run(query, values, afterInsertData)
   // consultar dados da tabela
 
   // deletar um dado da tabele
